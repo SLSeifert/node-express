5cc4b39032b14593ebace47fd60d421900db82c5
SLSeifert <slseifert1986@gmail.com>
2026-05-23 10:19:30 -0700

    Create partnersRouter module.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const campsiteRouter = require("./routes/campsiteRouter");
 const promotionsRouter = require("./routes/promotionsRouter");
+const partnersRouter = require("./routes/partnersRouter");
 
 const hostname = "localhost";
 const port = 3000;
@@ -12,6 +13,7 @@ app.use(express.json());
 
 app.use("/campsites", campsiteRouter);
 app.use("/promotions", promotionsRouter);
+app.use("/partners", partnersRouter);
 
 app.use(express.static(__dirname + "/public"));
 
